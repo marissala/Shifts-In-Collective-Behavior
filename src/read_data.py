4e4805ae22519c66c1428a8bdbcc4ffffe85256f
marissala <marissala.o7@gmail.com>
2021-12-14 13:31:10 +0100

    cleaned up and updated with current

--- src/read_data.py
+++ src/read_data.py
@@ -12,95 +12,67 @@ from icecream import ic
 
 # dk_groups_clean.json  dk_groups_comments_clean.json  dk_groups_posts_clean.json
 
-#filenames = glob.glob(data_path)
-#pattern = r'}(, )'
-
-
-
-def read_data(filename):
-    with open(filename) as f:
-        #lines = [next(f) for x in range(1000000)]
-        #lines = f.read().splitlines("}(, ){") #f.readlines()
-        giant_string = f.read()
-    giant_string = giant_string.replace('\n', '\\n') #fix 1897017
-    giant_string = giant_string.replace('}\s+{\s+"id"' , '}, \\n{ "id"') #fix 41432
-    giant_string = giant_string.replace(' \\n}, { "id"', '}, \\n{ "id"') #fix 186777
-    giant_string = giant_string.replace('\s+},\s+{\s+"id"', '}, \\n{ "id"') #fix 186803
-    #fix 430987  }, {  "id"
-    giant_string = giant_string.replace('"application":null', '"application":"null"')
-    giant_string = giant_string.replace('}, \\n{ "id"', '}, splitstringhere\\n{ "id"') #if just this then we get 512763 lines, with the upper one it has 512763 lines, same same
-    pre_lines = giant_string.split('splitstringhere\\n')
-    del giant_string
-    lines = [re.sub("\\\\n", " ", line) for line in pre_lines]#[line.strip('\\\\n') for line in pre_lines]
-    #lines = [re.sub("\\n", " ", line) for line in lines1]
-    del pre_lines
-    print(len(lines))
-    print(type(lines))
-    print(repr(lines[0]))
+def read_tmp_data(data_path: str):
+    """Reads a temporary smaller dataset for testing
+    Args:
+    data_path: complete path of temporary dataset (.csv)
     
-    #pattern = r" },"
-    #lines2 = [re.sub(" },", "}", line) for line in lines]
-    #lines21 = [re.sub(':null', ':"null"', line) for line in lines]
-    #lines22 = [re.sub('} {', '}, {', line) for line in lines21]
-    #lines2 = [re.sub('" \n', '"}', line) for line in lines22]
-    #lines22 = [re.sub('^}, {', '{', line) for line in lines2]
-    #lines21 = [re.sub('\n', '', line) for line in lines22]
-    #print(lines21[0])
-    #ic(type(lines21[0]))
-
-    i = 0
-    mistakes = 0
-    lines3 = []
-    for line in lines:
-        try:
-            lines3.append(ast.literal_eval(line))
-            i = i+1
-        except:
-            print("error in line: ", i)
-            print(repr(line[0:10000]))
-            #nextline=i+1
-            #print("-----------------------------")
-            #print("next line: ")
-            #print(repr(lines[nextline][0:5000]))
-            
-            #print("-----------------------------")
-            #print("previous line: ")
-            #previousline=i-1
-            #print(repr(lines[previousline][0:5000]))
-            print(i)
-            i = i+1
-            mistakes = mistakes+1
-            if mistakes > 10:
-                print(maris)
-            else:
-                continue
-            
-    del lines
-    print(f"NUMBER OF MISTAKES: {mistakes}")
-    #lines3 = [ast.literal_eval(line) for line in lines2]
-    df = pd.DataFrame.from_records(lines3)#, index=True)
-    del lines3
-    print(f"LENGTH OF DF: ", len(df))
+    Returns:
+    pandas DataFrame
+    """
+    df = pd.read_csv(data_path, sep=";")
     return df
 
-"""
-for filename in filenames:
-    ic(filename)
+def read_json_data(filename: str):
+    """Reads large json file that has problems with newlines
+    Args:
+    filename: complete path to json file
+
+    Returns:
+    pandas DataFrame
+    """
+    ic("[INFO] Read string")
     with open(filename) as f:
-        lines = f.read().splitlines() #f.readlines()
-    ic(filename)
-    ic(len(lines))
-    hmm = re.sub(pattern, "}", lines[0])
-    hmm2 = ast.literal_eval(hmm)
-    df = pd.DataFrame.from_records([hmm2])#, index=True)
-    ic(df.columns)
-"""
+        giant_string = f.read()
+    giant_string = giant_string[:-5]
+    
+    ic("[INFO] Base cleaning")
+    giant_string = re.sub('\\n', ' ', giant_string)
+    giant_string = f'[{giant_string}]'
+    
+    ic("[INFO] Json loads")
+    data = json.loads(giant_string)
+    del giant_string
+    ic("[INFO] Convert to pandas df")
+    df = pd.DataFrame.from_records(data)
+    return df
+
+def readData(filename: str, 
+              from_originals: bool):
+    """Reads in the original large json file, or a smaller saved sample
+    Args:
+    filename: complete path to json file
+    from_originals: if user wants to load original json or a sample dataset
+    
+    Returns:
+    pandas DataFrame
+    """
+    ic("[START] Reading in data")
+    if from_originals:
+        df = read_json_data(filename)
+    else:
+        df = pd.read_csv("ds.csv")
+    if "text" not in df.columns:
+        df["text"] = df["message"]
+    ic("[END] Finished reading in data")
+    return df
+
 
 if __name__ == '__main__':
     ic("START PIPELINE---------------------------------")
     data_path = "/data/datalab/danish-facebook-groups/raw-export/*"
     filenames = glob.glob(data_path)
-    df = read_data(filenames[2])
+    df = readData(filenames[2])
     #dff = df[:100]
     #dff.to_csv("/home/commando/marislab/facebook-posts/tmp/tmp_df.csv", index = False, sep=";")
     ic(df.head())
